d5b6896fb301650119dabd6192200801de0396a0
ElQueTrabaja <alumnoalgorithmicszgz7@gmail.com>
2025-06-03 20:32:17 +0200

    Add files via upload

--- ping.py
+++ ping.py
@@ -8,7 +8,6 @@ class GameSprite(sprite.Sprite):
         sprite.Sprite.__init__(self)
 
         # cada objeto debe almacenar una propiedad image
-        print(player_image)
         self.image = transform.scale(image.load(player_image), (size_x, size_y))
         self.speed = player_speed
 
@@ -22,7 +21,9 @@ class GameSprite(sprite.Sprite):
         win.blit(self.image, (self.rect.x, self.rect.y))
 
 class Player1(GameSprite):
+    
     def update(self):
+        global vel_y, vel_x, count_1, count_2
         keys = key.get_pressed()
         if keys[K_UP] and self.rect.y > 5:
             self.rect.y -= self.speed
@@ -35,6 +36,7 @@ class Player1(GameSprite):
 
 class Player2(GameSprite):
     def update(self):
+        global vel_y, vel_x, count_1, count_2
         keys = key.get_pressed()
         if keys[K_w] and self.rect.y > 5:
             self.rect.y -= self.speed
@@ -46,24 +48,38 @@ class Player2(GameSprite):
         win.blit(wins2, (450, 50))
 
 class bola(GameSprite):
+    
     def update(self):
+        global vel_y, vel_x, count_1, count_2
         if self.rect.x <= 5:
             count_2 += 1
+            self.rect.x = 250
+            self.rect.y = 75
+            vel_y = 2
+            vel_x = 2
         if self.rect.x >= 695:
             count_1 += 1
+            self.rect.x = 250
+            self.rect.y = 75
+            vel_y = 2
+            vel_x = 2
+    def mov(self):
+        self.rect.x += vel_x
+        self.rect.y += vel_y          
 
 WIDTH, HEIGHT = 700, 500
 win = display.set_mode((WIDTH, HEIGHT))
 display.set_caption("Ping")
 count_1 = 0
 count_2 = 0
-vel = 2
+vel_y = 2
+vel_x = 2
 background = transform.scale(image.load('a.jpg'), (700, 500))
 font.init()
 font = font.SysFont('Arial', 50)
 play1 = Player1('b.jpg', 50, 250, 30, 150, 3)
 play2 = Player2('b.jpg', 600, 250, 30, 150, 3)
-pelota = bola('p.jpg', 350, 250, 75, 75, vel)
+pelota = bola('p.jpg', 350, 250, 75, 75, 0)
 
 clock = time.Clock()
 finish = False
@@ -80,5 +96,23 @@ while game != False:
         play2.reset()
         pelota.update()
         pelota.reset()
+        pelota.mov()
+        if sprite.collide_rect(pelota, play2):
+
+            vel_y += 1
+            vel_x += 1
+            vel_x *= -1            
+        if sprite.collide_rect(pelota, play1):
+
+            vel_y += 1
+            vel_x += 1
+            vel_x *= -1            
+        if pelota.rect.y >= 450:
+
+            vel_y *= -1            
+        if pelota.rect.y <= 50:
+
+            vel_y *= -1
     display.update()
     clock.tick(60)
+
